a153f6602c3e6370dca3fa398aebb132affcf535
Bernhard Loibl <bernhard.loibl@gmail.com>
2016-03-11 14:50:32 +0100

    Flips chat and guide content.

--- Guide.js
+++ Guide.js
@@ -2,7 +2,6 @@
 
 var React = require('react-native');
 var _styles = require('./styles.js');
-var GuideItem = require('./GuideItem.js');
 
 var {
   StyleSheet,
@@ -15,79 +14,19 @@ var {
   NavigatorIOS,
 } = React;
 
-var MOCKED_COLLECTION_DATA = [
-  {id: 1, title: 'One'},
-  {id: 2, title: 'Two'},
-  {id: 3, title: 'Three'},
-  {id: 4, title: 'Four'},
-  {id: 5, title: 'Five'},
-  {id: 6, title: 'Six'},
-];
-
 var styles = StyleSheet.create({
-  list: {
-    justifyContent: 'space-around',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    margin: 10,
-  },
-  row: {
-    justifyContent: 'center',
-    margin: 5,
-    width: 100,
-    height: 120,
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 18,
-    color: '#656565',
-    marginTop: 5
-  },
-  circle: {
-    height: 80,
-    width: 80,
-    borderRadius: 40,
-    backgroundColor: '#c373da',
+  container: {
+    flex: 1,
+    paddingTop: 64,
   },
 });
 
 class Guide extends Component {
-  constructor(props) {
-    super(props);
-    var dataSource = new ListView.DataSource(
-      {rowHasChanged: (r1, r2) => r1.id !== r2.id});
-    this.state = {
-      dataSource: dataSource.cloneWithRows(MOCKED_COLLECTION_DATA)
-    };
-  }
-
-  rowPressed() {
-    console.log('Row Pressed');
-  }
-
-  renderRow(rowData, sectionID, rowID) {
-    return (
-      <TouchableHighlight onPress={() => this.rowPressed()} underlayColor="transparent">
-        <View>
-          <View style={styles.row}>
-            <View style={styles.circle}>
-              <Image style={styles.thumb} source={{ uri: rowData.img_url }} />
-            </View>
-            <Text style={styles.title} numberOfLines={1}>{rowData.title}</Text>
-          </View>
-        </View>
-      </TouchableHighlight>
-    );
-  }
-
   render() {
-    return (
-      <ListView
-        contentContainerStyle={styles.list}
-        dataSource={this.state.dataSource}
-        pageSize={3}
-        scrollRenderAheadDistance={500}
-        renderRow={this.renderRow.bind(this)}/>
+    return(
+      <View style={styles.container}>
+        <Text>Guide</Text>
+      </View>
     );
   }
 }
