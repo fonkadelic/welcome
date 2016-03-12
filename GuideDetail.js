f742fbacd633150992e93f872f742172dd8b9f30
Bernhard Loibl <bernhard.loibl@gmail.com>
2016-03-12 10:42:43 +0100

    Updates / extends guide navigation.

--- GuideDetail.js
+++ GuideDetail.js
@@ -19,27 +19,39 @@ class GuideDetail extends Component {
 
   constructor(props) {
     super(props);
+
     var dataSource = new ListView.DataSource(
       {rowHasChanged: (r1, r2) => r1.id !== r2.id});
-    var filtered = props.guides.filter(prop => props.subIDs.includes(prop.id));  
+    var filtered = props.guides.filter(prop => props.subIDs.includes(prop.id));
 
     this.state = {
       dataSource: dataSource.cloneWithRows(filtered)
     };
   }
 
+  rowPressed(rowData) {
+    var subGuides = rowData.sub_guides;
+    if (subGuides.length > 0) {
+      this.props.navigator.push({
+          title: rowData.title,
+          component: GuideDetail,
+          passProps: {guides: this.props.guides, subIDs: subGuides}
+      });
+    }
+  }
+
   renderRow(rowData, sectionID, rowID) {
     return (
-      <GuideItem guide={rowData} />
+      <GuideItem guide={rowData} onPress={this.rowPressed.bind(this)} />
     );
   }
 
   render() {
     return (
-    <ListView
-      dataSource={this.state.dataSource}
-      renderRow={this.renderRow.bind(this)}/>
-  );
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow.bind(this)}/>
+    );
   }
 }
 
